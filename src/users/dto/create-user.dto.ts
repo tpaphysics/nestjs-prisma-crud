701fc9306bae3d3e117c885912fb229e4c035f57
tpaphysics <physics.thiago@gmail.com>
2022-04-12 05:23:50 -0300

    feat: finalização do crud de usuários com nestjs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends User {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
